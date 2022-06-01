ffe9bbf9417bdf6d7e231bcce80874a015914592
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-01 12:52:52 +0100

    update

--- components/client/bin/startup.sh
+++ components/client/bin/startup.sh
@@ -2,19 +2,20 @@
 
 routerip=`dig +noall +answer router.pocketsoc-ng_internal | awk '{print $NF}'`
 
-external_subnet="19"
+#external_subnet="19"
 internal_subnet="18"
 mirror_subnet="20"
 
 # Define interfaces
 
-external=`route | grep $external_subnet | awk '{print $NF}'`
+#external=`route | grep $external_subnet | awk '{print $NF}'`
 internal=`route | grep $internal_subnet | awk '{print $NF}'`
 mirror=`route | grep $mirror_subnet | awk '{print $NF}'`
 
-ip link set dev $external down
+#ip link set dev $external down
 ip link set dev $mirror down
 
-ip route add 172.19.0.0/16 via $routerip
+ip route del default
+ip route add 172.18.0.0/16 via $routerip
 
 tail -f /dev/null
