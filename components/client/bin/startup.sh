0a740252bf0beacadeb09ea15607cdabb3577389
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-01 13:17:02 +0100

    update

--- components/client/bin/startup.sh
+++ components/client/bin/startup.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
 routerip=`dig +noall +answer router.pocketsoc-ng_internal | awk '{print $NF}'`
+webserverip=`dig +noall +answer webserver.pocketsoc-ng_internal | awk '{print $NF}'`
 
-#external_subnet="19"
+# Define subnets
+
 internal_subnet="18"
 mirror_subnet="20"
 
 # Define interfaces
 
-#external=`route | grep $external_subnet | awk '{print $NF}'`
-internal=`route | grep $internal_subnet | awk '{print $NF}'`
-mirror=`route | grep $mirror_subnet | awk '{print $NF}'`
+internal_interface=`route | grep $internal_subnet | awk '{print $NF}'`
+mirror_interface=`route | grep $mirror_subnet | awk '{print $NF}'`
 
-#ip link set dev $external down
-ip link set dev $mirror down
+ip link set dev $mirror_interface down
 
-ip route del default
-ip route add 172.18.0.0/16 via $routerip
+ip route flush $internal_interface
+ip route add 172.18.0.0/16 dev $internal_interface
+ip route add $webserverip via $routerip
 
 tail -f /dev/null
