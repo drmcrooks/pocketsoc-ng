6f33b7ab0a670bdc755afd0b7fd15aaaa1250d6d
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-02 12:37:14 +0100

    update

--- components/client/bin/startup.sh
+++ components/client/bin/startup.sh
@@ -6,14 +6,10 @@ webserverip=`dig +noall +answer webserver.pocketsoc-ng_internal | awk '{print $N
 # Define subnets
 
 internal_subnet="18"
-mirror_subnet="20"
 
 # Define interfaces
 
 internal_interface=`route | grep $internal_subnet | awk '{print $NF}'`
-mirror_interface=`route | grep $mirror_subnet | awk '{print $NF}'`
-
-ip link set dev $mirror_interface down
 
 ip route flush $internal_interface
 ip route add 172.18.0.0/16 dev $internal_interface
--- components/webserver/bin/startup.sh
+++ components/webserver/bin/startup.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
 
 routerip=`dig +noall +answer router.pocketsoc-ng_internal | awk '{print $NF}'`
-clientip=`dig +noall +answer client.pocketsoc-ng_internal | awk '{print $NF}'`
+clientip=`dig +noall +answer webserver.pocketsoc-ng_internal | awk '{print $NF}'`
 
 # Define subnets
 
 internal_subnet="18"
-mirror_subnet="20"
 
 # Define interfaces
 
 internal_interface=`route | grep $internal_subnet | awk '{print $NF}'`
-mirror_interface=`route | grep $mirror_subnet | awk '{print $NF}'`
-
-ip link set dev $mirror_interface down
 
 ip route flush $internal_interface
 ip route add 172.18.0.0/16 dev $internal_interface
