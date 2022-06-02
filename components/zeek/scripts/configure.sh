6607b6c4ad1ae8775c56469dc2cca8e5c50bcf80
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-02 15:34:13 +0100

    update

--- components/zeek/scripts/configure.sh
+++ components/zeek/scripts/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ETC_PATH=/usr/local/zeek/etc/
+ETC_PATH=/opt/zeek/etc/
+LOCAL_PATH="/opt/pocketsoc-ng"
 WORK_DIR=$1
 
 if [ ! -d "${ETC_PATH}" ]; then
@@ -8,14 +9,14 @@ if [ ! -d "${ETC_PATH}" ]; then
 fi
 
 if [ ! -f "${ETC_PATH}/networks.cfg" ]; then
-  cp /code/config/networks.cfg ${ETC_PATH}
+  cp $LOCAL_PATH/code/config/networks.cfg ${ETC_PATH}
 fi
 
 if [ ! -f "${ETC_PATH}/node.cfg" ]; then
-  cp /code/config/node.cfg ${ETC_PATH}
+  cp $LOCAL_PATH/code/config/node.cfg ${ETC_PATH}
 fi
 
 if [ ! -f "${ETC_PATH}/zeekctl.cfg" ]; then
-  cp /code/config/zeekctl.cfg ${ETC_PATH}
+  cp $LOCAL_PATH/code/config/zeekctl.cfg ${ETC_PATH}
 fi
 
