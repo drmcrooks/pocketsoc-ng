8407fc873405de7cf7b4bac8670d702b629a76e1
David Crooks <david.crooks@stfc.ac.uk>
2022-06-02 15:00:54 +0100

    update

--- components/zeek/scripts/configure.sh
+++ components/zeek/scripts/configure.sh
@@ -8,14 +8,14 @@ if [ ! -d "${ETC_PATH}" ]; then
 fi
 
 if [ ! -f "${ETC_PATH}/networks.cfg" ]; then
-  cp /code/Zeek/config/networks.cfg ${ETC_PATH}
+  cp /code/config/networks.cfg ${ETC_PATH}
 fi
 
 if [ ! -f "${ETC_PATH}/node.cfg" ]; then
-  cp /code/Zeek/config/node.cfg ${ETC_PATH}
+  cp /code/config/node.cfg ${ETC_PATH}
 fi
 
 if [ ! -f "${ETC_PATH}/zeekctl.cfg" ]; then
-  cp /code/Zeek/config/zeekctl.cfg ${ETC_PATH}
+  cp /code/config/zeekctl.cfg ${ETC_PATH}
 fi
 
