e91db5893a739025142c9699f40b8fe978988b88
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-04 12:46:20 +0100

    Update startup.sh

--- components/client/bin/startup.sh
+++ components/client/bin/startup.sh
@@ -1,19 +1,6 @@
 #! /bin/bash
 
- routerip=`dig +noall +answer router.pocketsoc-ng_internal | awk '{print $NF}'`
- webserverip=`dig +noall +answer webserver.pocketsoc-ng_internal | awk '{print $NF}'`
- 
- # Define subnets
-
- internal_subnet="18"
-
- # Define interfaces
-
- internal_interface=`route | grep $internal_subnet | awk '{print $NF}'`
-
- ip route flush $internal_interface
- ip route del default
- ip route add 172.18.0.0/16 dev $internal_interface
- ip route add $webserverip via $routerip
+chmod +x /opt/pocketsoc-ng/bin/routing.sh
+/opt/pocketsoc-ng/bin/routing.sh
 
 tail -f /dev/null
