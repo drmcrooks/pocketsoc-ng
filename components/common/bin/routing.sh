48bea81b1dcdd01127dc99f9584d76c56b74bb80
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-04 12:42:35 +0100

    Update routing.sh

--- components/common/bin/routing.sh
+++ components/common/bin/routing.sh
@@ -10,6 +10,5 @@
 
  internal_interface=`ip route | grep default | awk '{print $NF}'`
 
- ip route flush dev $internal_interface
  ip route add 172.18.0.0/24 dev $internal_interface
  ip route change 172.18.0.0/24 via $routerip
