d0dd51b55562f865679770529712a7634f472dc2
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-04 21:01:51 +0100

    Update generate_keys.sh

--- components/misp-reverse-proxy/bin/generate_keys.sh
+++ components/misp-reverse-proxy/bin/generate_keys.sh
@@ -2,6 +2,6 @@
 
 key_location="/usr/local/apache2/conf/cert"
 
-openssl req -new -x509 -nodes -out $key_location/server.crt -keyout $key_location/server.key -subj '/CN=localhost`
+openssl req -new -x509 -nodes -out $key_location/server.crt -keyout $key_location/server.key -subj '/CN=localhost'
 
 ls $key_location
