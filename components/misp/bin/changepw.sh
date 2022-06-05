b0e70789a560fd13a17b1eb6f1a6f2eba8367edf
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-05 14:33:16 +0100

    Update changepw.sh

--- components/misp/bin/changepw.sh
+++ components/misp/bin/changepw.sh
@@ -2,4 +2,5 @@
 
 while [ ! -f /root/changemisppw.log ]; do
     /var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD} --no_password_change > /root/changemisppw.log
+    sleep 1
 done
