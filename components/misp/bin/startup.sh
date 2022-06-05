41b599c6afbd947bd97c8b09f9f32a5a22886f3d
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-05 14:22:22 +0100

    Update startup.sh

--- components/misp/bin/startup.sh
+++ components/misp/bin/startup.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-sed -i "s/'require_password_confirmation' => true/'require_password_confirmation' => false/g" /var/www/MISP/app/Config/config.php
-
-/var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD} > /root/changepw
+/var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD} --no_password_change > /root/changepw
