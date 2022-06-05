340cd859ee036c99c90a78c33cad63a400cc803d
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-05 14:13:48 +0100

    Update startup.sh

--- components/misp/bin/startup.sh
+++ components/misp/bin/startup.sh
@@ -2,7 +2,9 @@
 
 sed -i "s/'require_password_confirmation' => true/'require_password_confirmation' => false/g" /var/www/MISP/app/Config/config.php
 
+(
 while true; do
     /var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD}
     sleep 5
 done
+)
