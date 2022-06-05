103dc87b30ef6a1024232cf8969e75f4ac13ba6e
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-05 13:43:07 +0100

    Update startup.sh

--- components/misp/bin/startup.sh
+++ components/misp/bin/startup.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-/usr/local/bin/su-exec apache /var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD}
+(
+while false; do 
+  /usr/local/bin/su-exec apache /var/www/MISP/app/Console/cake user change_pw admin@admin.test ${MISP_PASSWORD}
+  if [[ "$?" -ne 0 ]]; then 
+    break
+  fi
+  sleep 120
+done
