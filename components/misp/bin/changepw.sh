467d4481646228d9d8fbc2172a44030f5016caae
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-06 15:47:37 +0100

    Update changepw.sh

--- components/misp/bin/changepw.sh
+++ components/misp/bin/changepw.sh
@@ -3,7 +3,7 @@
 password=${MISP_PASSWORD}
 
 while [ ! -f /root/changemisppw.log ]; do
-    curl -k -L http://localhost/UserInitShell.php
-    su-exec apache /var/www/MISP/app/Console/cake user change_pw admin@admin.test "$password" --no_password_change > /root/changemisppw.log
+    curl -s -k -L http://localhost/UserInitShell.php > /dev/null
+    su-exec apache /var/www/MISP/app/Console/cake user change_pw admin@admin.test "$password" --no_password_change && touch /root/changemisppw.log
     sleep 10
 done
