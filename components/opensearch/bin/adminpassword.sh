ed06766528c91c809cd167f32a9e805786d5da84
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-09 12:04:18 +0100

    Update adminpassword.sh

--- components/opensearch/bin/adminpassword.sh
+++ components/opensearch/bin/adminpassword.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-echo hello
+passwordhash=`echo $ELASTIC_PASSWORD | xargs -I {} /usr/share/opensearch/plugins/opensearch-security/tools/hash.sh -p {}`
+
+sed -i "s/$2y$12$zxWlhOWRZelXrCm1AJrdPeKn8kagU3sqVKPQYN.QGXlsKvj5h.Xxe/$passwordhash/g" /usr/share/opensearch/config/opensearch-security/internal_users.yml
+
+grep $passwordhash /usr/share/opensearch/config/opensearch-security/internal_users.yml
