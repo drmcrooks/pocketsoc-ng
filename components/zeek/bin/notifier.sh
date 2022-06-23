ab91e0f57a742ab799722ebe1f3949d8626e9949
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-23 12:06:40 +0200

    Update notifier.sh

--- components/zeek/bin/notifier.sh
+++ components/zeek/bin/notifier.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 MESSAGE=$1
+ZEEKHOST=`cat /opt/pocketsoc-ng/data/zeekhost`
 WEBHOOK=`cat /opt/pocketsoc-ng/data/webhook`
 
-curl -i -X POST --data-urlencode "payload={\"text\": \"${MESSAGE}\"}" $WEBHOOK
+curl -i -X POST --data-urlencode "payload={\"text\": \"${ZEEKHOST}: ${MESSAGE}\"}" $WEBHOOK
