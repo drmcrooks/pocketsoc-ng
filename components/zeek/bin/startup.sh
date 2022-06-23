8dabac015983f53523e4582c2db8c979fb7abded
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-23 10:27:34 +0200

    Update startup.sh

--- components/zeek/bin/startup.sh
+++ components/zeek/bin/startup.sh
@@ -5,6 +5,8 @@ chmod +x /opt/pocketsoc-ng/bin/nic_setup.sh
 
 chmod +x /opt/pocketsoc-ng/bin/pull_misp.sh
 
+chmod +x /opt/pocketsoc-ng/bin/notifier.sh
+
 which supervisord
 
 /usr/bin/supervisord -c /etc/supervisord.conf
