64d8932b9b2d527fd3e7b4465425f3b4ed22c4c8
drmcrooks <David.crooks@stfc.ac.uk>
2023-10-12 05:02:08 +0200

    tidy

--- components/client/bin/startup.sh
+++ components/client/bin/startup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-chmod +x /opt/pocketsoc-ng/bin/routing.sh
-#/opt/pocketsoc-ng/bin/routing.sh
+chmod +x /opt/pocketsoc-ng/bin/mirror.sh
+/opt/pocketsoc-ng/bin/mirror.sh
 
 tail -f /dev/null
--- components/router/bin/startup.sh
+++ components/router/bin/startup.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-chmod +x /opt/pocketsoc-ng/bin/mirror.sh
-/opt/pocketsoc-ng/bin/mirror.sh
-
 tail -f /dev/null
